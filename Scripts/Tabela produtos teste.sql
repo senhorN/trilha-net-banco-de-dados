f37799d5cb52765b13f88111b84e3b3a2fc9ab77
Nicolas Carloto <90424448+senhorN@users.noreply.github.com>
2022-12-30 13:44:03 -0300

    Update Tabela produtos teste.sql

--- Scripts/Tabela produtos teste.sql
+++ Scripts/Tabela produtos teste.sql
@@ -4,17 +4,17 @@ select * from Produtos --Count -> serve como contador de linhas
 alter table Produtos --Altera a coluna da tabela
 add DataCadastro datetime2 -- add uma coluna chamada Datacadastro
 
---alter table produtos 
---drop column DataCadastro -- remove uma coluna  
+alter table produtos 
+drop column DataCadastro -- remove uma coluna  
 
 update  Produtos set DataCadastro = GETDATE()
 
 
 
---select sum(Preco) PrecoTotal from Produtos -- o SUM soma uma coluna na tabela 
+select sum(Preco) PrecoTotal from Produtos -- o SUM soma uma coluna na tabela 
 
---select min(preco) PrecoMenor from Produtos --MIN menor preco da tabela 
+select min(preco) PrecoMenor from Produtos --MIN menor preco da tabela 
 
---select max(preco) PrecoMaior from Produtos -- MAX maior preco da tabela 
+select max(preco) PrecoMaior from Produtos -- MAX maior preco da tabela 
 
---select avg(preco)  from Produtos
+select avg(preco)  from Produtos
